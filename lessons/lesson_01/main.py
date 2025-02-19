846e0fa2f0bd6d96bf12beb401bb459f80770e0a
NagyIstvanGabor <63602487+NagyIstvanGabor@users.noreply.github.com>
2025-02-19 16:43:00 +0100

    initial commit, adding folder structure, .gitignore, requirements.txt, readme, 1st lesson modules

---
+++ lessons/lesson_01/main.py
@@ -0,0 +1,36 @@
+print("Hello World!")
+
+# this is a comment / ez egy komment SINGLE LINE COMMENT
+
+print("hello world again!")
+
+
+"""
+THIS IS A MULTI
+LINE COMMENT
+"""
+
+'''
+THIS IS A MULTI
+LINE COMMENT
+'''
+
+print("hello world again!!!")
+
+
+#PRIMITIVE DATA TYPES / PRIMITÍV ADATTÍPUSOK
+
+print("apple") #STRING /SZÖVEG
+print("3234534") #STRING
+
+print(0) #INTEGER
+print(1) #INTEGER
+print(123123123132131231231) #INTEGER
+
+print(3.14) #FLOATING POINT NUMBER / LEBEGŐPONTOS SZÁM
+
+print(True) #BOOLEAN
+print(False) #BOOLEAN
+
+#NONE TYPE
+print(None) #információ hiányát jelenti
---
+++ lessons/lesson_01/virtual_env_practice.py
@@ -0,0 +1,9 @@
+#STANDARD LIBARY
+import random
+
+print(random.randint(1,10))
+
+#PYPI-PYTHON PACKAGE INDEX
+import fastapi
+
+app = fastapi.FastAPI()
