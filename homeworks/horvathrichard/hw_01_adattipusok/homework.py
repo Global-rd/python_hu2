81b6cd2b2d78e50397178cc3ebd74b648e2c41d4
Reechardo <ritchoo96@gmail.com>
2025-02-21 16:30:57 +0100

    first hw done

---
+++ homeworks/horvathrichard/hw_01_adattipusok/homework.py
@@ -0,0 +1,17 @@
+"""
+This is my first Python
+program
+"""
+
+#Itt jönnek a példák a primitív adattípusokra:
+
+print("Ez egy mondat") #Bármit is írunk bele, ha idézőjelbe kerül, string lesz belőle, azaz szöveg
+
+print(135) #Ez az egész szám az integer
+
+print(3.141) #A nem egész számokat nevezzük floating point number-nek
+
+print(True) #Boolean
+print(False) #Boolean
+
+print(None) #Ez az információ hiányát jelenti
