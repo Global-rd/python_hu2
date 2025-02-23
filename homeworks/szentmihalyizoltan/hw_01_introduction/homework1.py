f743079d673b8b295ed5678b329060034fa74717
zoltansze <zoltanszentmihalyi@gmail.com>
2025-02-23 16:35:03 +0100

    homework1 done

---
+++ homeworks/szentmihalyizoltan/hw_01_introduction/homework1.py
@@ -0,0 +1,10 @@
+"""
+This is my first Python
+program
+"""
+
+print("String") #String
+print(1)        #Intiger
+print(3.14)     #Floating number
+print(True)     #Boolean
+print(None)     #None type
