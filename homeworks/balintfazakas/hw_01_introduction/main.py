2a1d7ac90523c54e10e46f2a458cdea34edca677
Tygor99 <tygor99@proton.me>
2025-02-23 16:08:00 +0100

    Update main.py

---
+++ homeworks/balintfazakas/hw_01_introduction/main.py
@@ -0,0 +1,9 @@
+""""This is my first Python program"""
+"""Fixing a typo"""
+print("Hello World!") #STRING
+print("apple") #STRING
+print(1) #INTEGER
+print(3.14) #FLOATING POINT NUMBER
+print(True) #BOOLEAN
+print(False) #BOOLEAN
+print(None) #NONE
