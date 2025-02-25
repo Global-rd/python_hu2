a90c9a63eade67445361d589675895567af5065a
RiksatBacsi <dani.varsanyi@gmail.com>
2025-02-25 17:22:19 +0100

    homework 1
    the introduction

---
+++ homeworks/varsanyidaniel/homework-1_introduciton/homework.py
@@ -0,0 +1,11 @@
+"""
+ This is
+ the first time
+ im writing
+ 'Python Program'
+ in a multiline comment
+"""
+print("Üdvözletem!") #STRING
+print(True) #BOOFLEAN
+print(0) #INTEGER
+print(9.9999999999) #FLOATING POINT NUMBER
