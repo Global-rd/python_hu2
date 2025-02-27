52194f016abc10cfc0fe28519f9bd7cc15e08363
beresniki <beresniki1996@gmail.com>
2025-02-27 09:05:08 +0100

    homework 01 done

---
+++ homeworks/beresnikolett/hw_01_gitlearning/homework.py
@@ -0,0 +1,8 @@
+"""This is
+my first 
+python program."""
+print("sunshine") #string type
+print(29) #integer type
+print(235.7) #floating point number type
+print(True) #boolean type
+print(None) #none type (információ hiánya)
