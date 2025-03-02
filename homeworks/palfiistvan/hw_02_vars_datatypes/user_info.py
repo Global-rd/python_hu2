286d4b346aa47615c658cdc0a4b2dfba2aa638ea
palfiistvan68 <palfiistvan68@gmail.com>
2025-03-02 23:45:46 +0100

    Homework-2 is done

--- homeworks/palfiistvan/hw_02_vars_datatypes/user_info.py
+++ homeworks/palfiistvan/hw_02_vars_datatypes/user_info.py
@@ -57,6 +57,14 @@ user_info["phone_contacts"].pop("Tim")
 print(f"9. {user_info["phone_contacts"]}")
 
 # 10.
-#user_info["phone_contacts"].update({"Adam": "[+368099999, +3690999999]"})
 user_info["phone_contacts"].update({"Adam": ["+368099999", "+3690999999"]})
 print(f"10. {user_info["phone_contacts"]}")
+
+# Extra 1.
+Last3_list = list(user_info["skills"][-3:])
+Last3_list.reverse()
+print(Last3_list)
+
+# Extra 2.
+user_info["phone_contacts"]["Tim"] = user_info["phone_contacts"].pop("Tim2")
+print(user_info["phone_contacts"])
