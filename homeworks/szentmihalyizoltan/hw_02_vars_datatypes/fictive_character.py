c9e3fc79b9a33429852e6a9cd39049162eb29508
zoltansze <zoltanszentmihalyi@gmail.com>
2025-03-03 17:23:18 +0100

    homework 2 updated

--- homeworks/szentmihalyizoltan/hw_02_vars_datatypes/fictive_character.py
+++ homeworks/szentmihalyizoltan/hw_02_vars_datatypes/fictive_character.py
@@ -1,18 +1,21 @@
-fictive_character = {
-    "name" : "  John Doe  ",
-    "age" : 36,
-    "python_knowledge" : 2,
-}
+from pprint import pprint
+
+fictive_character = ()
+
+fictive_character_name = input("Please give a name to the character: ")
+fictive_character_age = input("Please give an age to the character: ")
+fictive_character_pk = input("Please give a python knowledge in years to the character: ")
+print("Name: " + fictive_character_name + ", "+ "Age: " + fictive_character_age + ", " + "Python knowledge: " + fictive_character_pk)
+print("------------------------------")
 
 #Kiemelem az életkort, amit megszorzok 365, és hozzáadom a szökpéveket, mivel az osztás miatt float-ba rakja a szökőévet átalakítom int-be
-age_in_day = fictive_character["age"] * 365
-leap_year = fictive_character["age"] / 4
+age_in_day = fictive_character_age * 365
+leap_year = fictive_character_age / 4
 age_in_day = age_in_day + int(leap_year)
 
-#Kiemelem a nevet, majd az upper-el nagybetüssé alakítom, és strip-el leveszem a szóközöket
-print(f"My character is {age_in_day} old. His/her name is {fictive_character["name"].upper().strip()} and he/she has {fictive_character["python_knowledge"]} years experience.")
-
 #Bekérek egy választ, amennyiben "igen"-t kapok, a python developer akar lenni a karakter, minden más esetben nem
-question = str(input("Would you like your character to be a professional python developer? (Please only write 'yes' if he/she wants)"))
+question = input("Would you like your character to be a professional python developer? (Please only write 'yes' if he/she wants)")
 prof_python = "wants" if question == "yes" else "does not want" 
-print(f"He/she {prof_python} to be a Python developer!")
+
+#Kiemelem a nevet, majd az upper-el nagybetüssé alakítom, és strip-el leveszem a szóközöket
+pprint(f"My character is {age_in_day} old. His/her name is {fictive_character_name.upper().strip()} and he/she has {fictive_character_pk} years experience. He/she {prof_python} to be a Python developer!")
--- homeworks/szentmihalyizoltan/hw_02_vars_datatypes/user_info.py
+++ homeworks/szentmihalyizoltan/hw_02_vars_datatypes/user_info.py
@@ -17,7 +17,7 @@ user_info = {
 }
 
 #Bekérem a négy nyelvet
-planguage = str(input("Please give the 4 programming language, separetad by coma: "))
+planguage = input("Please give the 4 programming language, separetad by coma: ")
 
 #A bekért nyelveket listává alakítom, amit a "skill"-ek közé felveszek 
 planguage_list = planguage.split(",")
@@ -27,13 +27,13 @@ user_info["skills"] = planguage_list
 pprint(user_info)
 print("------------------------------------------")
 
-#Megcserélem a kaják sorrendjét
-user_info["favourite_meals"].reverse()
+#ABC sorrendbe állítom a kajákat
+user_info["favourite_meals"].sort()
 pprint(user_info["favourite_meals"])
 print("------------------------------------------")
 
-#Kipprintelem a második elemet
-pprint(user_info["favourite_meals"][1])
+#Kipprintelem a utolsó előtti elemet
+pprint(user_info["favourite_meals"][-2])
 print("------------------------------------------")
 
 #Hozzáadom a spagettit
@@ -41,21 +41,18 @@ user_info["favourite_meals"].append("spagetthi")
 pprint(user_info["favourite_meals"])
 print("------------------------------------------")
 
-#Hozzáadom az utolsó 2 elemet
-user_info["favourite_meals"].extend(user_info["favourite_meals"][2:])
+#Hozzáadom a 3. és 4. elemet
+user_info["favourite_meals"].extend(user_info["favourite_meals"][2:4])
 pprint(user_info["favourite_meals"])
 print("------------------------------------------")
 
-#Elveszem az utolsó két elemet
-del user_info["favourite_meals"][-2:]
+#Törlöm a duplikátumokat
+user_info["favourite_meals"] = list(set(user_info["favourite_meals"]))
 pprint(user_info["favourite_meals"])
 print("------------------------------------------")
 
 #Felcserélem az első és utolsó elemet
-temp = user_info["favourite_meals"][0]
-user_info["favourite_meals"][0] = user_info["favourite_meals"][-1]
-user_info["favourite_meals"][-1] = temp
-pprint(user_info["favourite_meals"])
+user_info["favourite_meals"][0], user_info["favourite_meals"][-1] = user_info["favourite_meals"][-1], user_info["favourite_meals"][0]
 print("------------------------------------------")
 
 #Hozzáadom az új embert a kontaktokhoz
@@ -69,18 +66,15 @@ pprint((user_info["phone_contacts"]))
 print("------------------------------------------")
 
 #Hozzáadom az új embert a két telefonszámmal a kontaktokhoz
-user_info["phone_contacts"]["Scarlet"] = "+363045624235", "+36302432332113"
+user_info["phone_contacts"]["Scarlet"] = ("+363045624235", "+36302432332113")
 pprint((user_info["phone_contacts"]))
 print("------------------------------------------")
 
 #Kiprintelem a 3 utolsó skill-t fordított sorrendben
-reversed_skill = user_info["skills"][-3:]
-reversed_skill.reverse()
-pprint(reversed_skill)
+print(user_info["skills"][-1:-4:-1])
 print("------------------------------------------")
 
 #Kicserélem Tim2-t Tim-re
-user_info["phone_contacts"]["Tim"] = user_info["phone_contacts"]["Tim2"]
-del user_info["phone_contacts"]["Tim2"]
+user_info["phone_contacts"]["Tim"] = user_info["phone_contacts"].pop("Tim2")
 pprint((user_info["phone_contacts"]))
 print("------------------------------------------")
