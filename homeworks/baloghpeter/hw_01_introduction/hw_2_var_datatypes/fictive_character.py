9ddea4b2d1a4a8c7ac477f91e11914bdd265176c
szotyi0523 <szotyi0523@gmail.com>
2025-03-04 16:43:02 +0100

    hw_2_update

--- homeworks/baloghpeter/hw_01_introduction/hw_2_var_datatypes/fictive_character.py
+++
@@ -1,6 +0,0 @@
-name = input("What is your name: " ).lstrip().upper()
-age = int(input("How old are you: " ))
-python_exp_in_years = int(input("How many years of python programming experience do you have: "  ))
-ageindays = age * 365
-print("---------------")
-print(f"My character is {ageindays} old. His name is {name} and he has {python_exp_in_years} years expereince.")
