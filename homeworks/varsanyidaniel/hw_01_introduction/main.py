b225c00608a1c3a3d4eee4e32d816dfe50e5a0d9
RiksatBacsi <dani.varsanyi@gmail.com>
2025-03-05 22:25:56 +0100

    homework 1 pótlása
    minden jó

---
+++ homeworks/varsanyidaniel/hw_01_introduction/main.py
@@ -0,0 +1,11 @@
+"""
+this is a multiline
+comment
+as you can see
+"""
+
+print("almafás almafák")          #STRING
+print(1234442426523632)           #INTEGER
+print(4314.412521599999)          #FLOATING POINT NUMBER
+print(True)                       #BOOLEAN
+print(None)                       #NONE
