4feb4e9d6592c7b07c038a0946414feb5d606f5c
Rostás Péter <petipro3@gmail.com>
2025-03-05 18:21:42 +0100

    javítva

--- homeworks/rostaspeter/hw_02_vars_datatypes/fictive_character.py
+++ homeworks/rostaspeter/hw_02_vars_datatypes/fictive_character.py
@@ -5,9 +5,5 @@ experience = int(input("Hány év tapasztalatod van Pythonban?  "))
 age_in_days = age*365
 
 #szorgalmi
-want_to = True if input("Szeretnél professzionális Pythonista lenni? yes vagy no  ") == "yes" else False
-
-if want_to:
-    print(f"My character is {age_in_days} days old. His/her name is {name} and he/she has {experience} years experience. He/she wants to be a Python developer!")
-else:
-    print(f"My character is {age_in_days} days old. His/her name is {name} and he/she has {experience} years experience. He/she does not wants to be a Python developer!")
+want_to = "wants" if input("Szeretnél professzionális Pythonista lenni? yes vagy no ") == "yes" else "does not want"
+print(f"My character is {age_in_days} days old. His/her name is {name} and he/she has {experience} years experience. He/she {want_to} to be a Python developer!")
