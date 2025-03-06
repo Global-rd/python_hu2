0e7eed893ad9bf89999c88ed842a89c00e31c4bf
palfiistvan68 <palfiistvan68@gmail.com>
2025-03-06 15:34:20 +0100

    OK! Homework is done.

--- homeworks/palfiistvan/hw_02_vars_datatypes/fictive_character_ext.py
+++ homeworks/palfiistvan/hw_02_vars_datatypes/fictive_character_ext.py
@@ -4,17 +4,14 @@ name = input("What is your name? ").strip().capitalize()
 age = int(input("How old are you? "))
 age_in_days = age * 365
 experience = int(input("years of experience? "))
-profi = input("Do you want to become a professional Python developer? (yes/no)")
-if profi == "yes":
-    profi.replace("yes", "wants")
-if profi == "no":
-    profi.replace("no", "does not want")
+dev_motivation = input("Do you want to become a professional Python developer? (yes/no)")
+dev_intention = "wants" if dev_motivation == "yes" else "does not want"
 
 # print(name)
 # print(age)
 # print(age_in_days)
 # print(experience)
-# print(profi)
+# print(dev_motivation)
 
-my_character = f"My character is {age_in_days} days old. His/her name is {name} and he/she has {experience} years experience. He/she {profi} to be a Python developer!"
+my_character = f"My character is {age_in_days} days old. His/her name is {name} and he/she has {experience} years experience. He/she {dev_intention} to be a Python developer!"
 print(my_character)
