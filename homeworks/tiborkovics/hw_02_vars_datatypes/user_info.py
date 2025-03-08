a510fc07f6b02379d649fb108bcb9939226284c4
tiborkovics <tibor.kovics@gmail.com>
2025-03-08 18:41:34 +0100

    tiborkovicsHW02update

--- homeworks/tiborkovics/hw_02_vars_datatypes/user_info.py
+++ homeworks/tiborkovics/hw_02_vars_datatypes/user_info.py
@@ -20,27 +20,38 @@ user_info = {
 
 languages = input("Kerlek adj meg 4 prog nyelvet").split(",")   # 1/1
 
-skills = list[languages] # 1/2
-
-languages = user_info["skills"] # 1/3
+user_info["skills"] = languages # 1/2
+print(user_info)
 
 user_info["favourite_meals"].sort()  # 2
+print(user_info)
 
-print(user_info["favourite_meals"][-1])  # 3
+print(user_info["favourite_meals"][-2])  # 3
+print(user_info)
 
 
 user_info["favourite_meals"].append("spaghetti") # 4
+print(user_info)
 
 user_info["favourite_meals"].append("favourite_meals"[2:4])  # 5
+print(user_info)
 
 
-user_info["favourite_meals"] = set(user_info["favourite_meals"])
+user_info["favourite_meals"] = list(set(user_info["favourite_meals"])) # 6
+print(user_info)
 
-user_info = list(user_info) # 6
+user_info["favourite_meals"][0], user_info["favourite_meals"][-1] = user_info["favourite_meals"][-1], user_info["favourite_meals"][0]
+# 7
+print(user_info)
 
-user_info["favourite_meals"][0]. # 7
+user_info["phone_contacts"]["Tibi"] = "+367002421010" # 8
+print(user_info)
 
+del user_info["phone_contacts"][1] # 9
+print(user_info)
 
+user_info["phone_contacts"]["Anyu"] = ["+36701455098" , "+36707884956"] # 10
+print(user_info)
 
 
 
