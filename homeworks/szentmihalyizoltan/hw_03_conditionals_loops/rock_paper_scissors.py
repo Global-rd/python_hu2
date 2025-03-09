bbe51e2e02a9a83002b8e152a5e3ce2c750f9ed7
zoltansze <zoltanszentmihalyi@gmail.com>
2025-03-09 13:18:34 +0100

    update to rock/paper/scissor

--- homeworks/szentmihalyizoltan/hw_03_conditionals_loops/rock_paper_scissors.py
+++ homeworks/szentmihalyizoltan/hw_03_conditionals_loops/rock_paper_scissors.py
@@ -11,7 +11,7 @@ while rounds % 2 == 0:
 for round in range(1, rounds+1):
     #bekérem a az első majd a második játékos válaszát, addig, amíg nem a megfelelőt adják meg
     while True:
-        player_1 = input("Please choose on of these: Rock | Paper | Scissor: ").capitalize()
+        player_1 = input("Player 1 please choose on of these: Rock | Paper | Scissor: ").capitalize()
         if player_1 in  ["Rock", "Paper", "Scissor"]: 
             player_1_answer = player_1    
             break
@@ -19,7 +19,7 @@ for round in range(1, rounds+1):
             print("Wrong answer. Please enter: Rock | Paper | Scissor")
 
     while True:
-        player_2 = input("Please choose on of these: Rock | Paper | Scissor: ").capitalize()
+        player_2 = input("Player 2 please choose on of these: Rock | Paper | Scissor: ").capitalize()
         if player_2 in ["Rock", "Paper", "Scissor"]:
             player_2_answer = player_2    
             break
@@ -28,19 +28,31 @@ for round in range(1, rounds+1):
 
     #megnézem, hogy melyik játékos győzött és pontot ad neki  
     if (player_1_answer == "Rock" and player_2_answer == "Scissor") or (player_1_answer == "Scissor" and player_2_answer == "Paper") or (player_1_answer == "Paper" and player_2_answer == "Rock"):
-        print("Player 1 won this round")     
         player_1_points += 1
     elif (player_2_answer == "Rock" and player_1_answer == "Scissor") or (player_2_answer == "Scissor" and player_1_answer == "Paper") or (player_2_answer == "Paper" and player_1_answer == "Rock"):
-        print("Player 2 won this round")
         player_2_points += 1    
-    else:
-        print("Draw")
+    while player_1 == player_2:
+        print("It's a draw, please choose again")
+        player_1 = input("Player 1 please  choose on of these: Rock | Paper | Scissor: ").capitalize()
+        if player_1 in  ["Rock", "Paper", "Scissor"]: 
+            player_1_answer = player_1    
+            player_1_points += 1
+        else:
+            print("Wrong answer. Please enter: Rock | Paper | Scissor")        
+        player_2 = input("Player 2 please choose on of these: Rock | Paper | Scissor: ").capitalize()
+        if player_2 in ["Rock", "Paper", "Scissor"]:
+            player_2_answer = player_2
+            player_2_points += 1 
+        else:
+            print("Wrong answer. Please enter: Rock | Paper | Scissor")  
+            if (player_1_answer == "Rock" and player_2_answer == "Scissor") or (player_1_answer == "Scissor" and player_2_answer == "Paper") or (player_1_answer == "Paper" and player_2_answer == "Rock"):
+                player_1_points += 1
+            elif (player_2_answer == "Rock" and player_1_answer == "Scissor") or (player_2_answer == "Scissor" and player_1_answer == "Paper") or (player_2_answer == "Paper" and player_1_answer == "Rock"):
+                player_2_points += 1       
 print("---------------")
 
 #összeadom a pontokat, hogy lássam ki nyert, és azt kiiratom
 if player_1_points > player_2_points:
     print(f"Player 1 won the game! The score: {player_1_points}")
-elif player_2_points > player_1_points:
+else:
     print(f"Player 2 won the game! The score: {player_2_points}")
-elif player_2_points == player_1_points:
-    print("It is a draw!")
