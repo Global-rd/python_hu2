e450e8c35b6a7194d86de5fb4644fd5333dff897
ErikaKartali <sziszmok02@gmail.com>
2025-03-11 15:20:51 +0100

    kő-papírban javítottam egy hibát

--- homeworks/kartalierika/hw_03_conditionals_loops/rock_paper_scissors.py
+++ homeworks/kartalierika/hw_03_conditionals_loops/rock_paper_scissors.py
@@ -13,19 +13,24 @@ points = {"Player 1": 0, "Player 2": 0}
 options = ["rock", "paper", "scissors"]
 
 for round_num in range(rounds):
-    p1 = input("Player 1's choice: ").lower()
-    while p1 not in options:
-        print("Invalid choice! Please choose between rock, paper, or scissors.")
-        p1 = input("Player 1's choice: ").lower()
+    print(f"\nRound {round_num + 1}:")
     
-    p2 = input("Player 2's choice: ").lower()
-    while p2 not in options:
-        print("Invalid choice! Please choose between rock, paper, or scissors.")
-        p2 = input("Player 2's choice: ").lower()
+    while True:
+        p1 = input("Player 1's choice: ")
+        while p1 not in options:
+            print("Invalid choice! Please choose between rock, paper, or scissors.")
+            p1 = input("Player 1's choice: ")
+        
+        p2 = input("Player 2's choice: ")
+        while p2 not in options:
+            print("Invalid choice! Please choose between rock, paper, or scissors.")
+            p2 = input("Player 2's choice: ")
+        
+        if p1 != p2:
+            break
+        print("Draw! Please choose again.")
     
-    if p1 == p2:
-        print("Draw.")
-    elif (p1, p2) in [("rock", "scissors"), ("scissors", "paper"), ("paper", "rock")]:
+    if (p1, p2) in [("rock", "scissors"), ("scissors", "paper"), ("paper", "rock")]:
         print("Player 1 wins this round!")
         points["Player 1"] += 1
     else:
@@ -40,5 +45,3 @@ else:
 print("--------------------------------")
 print(f"\nGame Over! {winner} won with: {points['Player 1']} - {points['Player 2']}")
 print("--------------------------------")
-
-
