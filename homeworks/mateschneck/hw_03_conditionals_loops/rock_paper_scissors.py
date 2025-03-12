4880724bf50934e6e1a386a6eaa00629a177f9fa
manzyka <mate.schneck@powerinnovate.hu>
2025-03-12 09:01:42 +0100

    javított 3

--- homeworks/mateschneck/hw_03_conditionals_loops/rock_paper_scissors.py
+++ homeworks/mateschneck/hw_03_conditionals_loops/rock_paper_scissors.py
@@ -2,7 +2,7 @@ def get_valid_rounds():
     while True:
         try:
             rounds = int(input("Hány kört akartok játszani? (páratlan szám): "))
-            if rounds % 2 == 1:
+            if rounds % 2 != 0:
                 return rounds
             else:
                 print("Hiba: páratlan számot adj meg!")
@@ -20,7 +20,7 @@ def get_valid_choice(player):
 
 def determine_winner(choice1, choice2):
     if choice1 == choice2:
-        return 0
+        return None  # döntetlen
     elif (choice1 == "rock" and choice2 == "scissors") or \
          (choice1 == "scissors" and choice2 == "paper") or \
          (choice1 == "paper" and choice2 == "rock"):
@@ -30,29 +30,38 @@ def determine_winner(choice1, choice2):
 
 def play_game():
     rounds = get_valid_rounds()
-    score1 = 0
-    score2 = 0
+    
+    while True:
+        score1 = 0
+        score2 = 0
+        
+        for _ in range(rounds):
+            print("\nÚj kör!")
+            choice1 = get_valid_choice("1. játékos")
+            choice2 = get_valid_choice("2. játékos")
+            winner = determine_winner(choice1, choice2)
 
-    for _ in range(rounds):
-        print("\nÚj kör!")
-        choice1 = get_valid_choice("1. játékos")
-        choice2 = get_valid_choice("2. játékos")
-        winner = determine_winner(choice1, choice2)
+            if winner == 1:
+                print("1. játékos nyerte ezt a kört!")
+                score1 += 1
+            elif winner == 2:
+                print("2. játékos nyerte ezt a kört!")
+                score2 += 1
+            else:
+                print("Döntetlen, senki nem kap pontot!")
 
-        if winner == 1:
-            print("1. játékos nyerte ezt a kört!")
-            score1 += 1
-        elif winner == 2:
-            print("2. játékos nyerte ezt a kört!")
-            score2 += 1
+        print("\nJáték vége!")
+        if score1 > score2:
+            print(f"Az 1. játékos nyert {score1} ponttal!")
+            break
+        elif score2 > score1:
+            print(f"A 2. játékos nyert {score2} ponttal!")
+            break
         else:
-            print("Döntetlen!")
-
-    print("\nJáték vége!")
-    if score1 > score2:
-        print(f"Az 1. játékos nyert {score1} ponttal!")
-    else:
-        print(f"A 2. játékos nyert {score2} ponttal!")
+            print("A játék döntetlennel zárult! Döntő kör következik!")
+            # reset a döntő körhöz
+            score1 = 0
+            score2 = 0
 
 if __name__ == "__main__":
     play_game()
