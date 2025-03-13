572d04e9449e56382a4df0b434caff41a5b666dd
RiksatBacsi <dani.varsanyi@gmail.com>
2025-03-13 18:42:11 +0100

    homework 3 fix
    added the extra while True loop

--- homeworks/varsanyidaniel/hw_03_conditionals_loops/rock_paper_scissors.py
+++ homeworks/varsanyidaniel/hw_03_conditionals_loops/rock_paper_scissors.py
@@ -7,28 +7,30 @@ while rounds%2 == 0:
 for i in range(rounds):
 
     while True:
-        first_player_input=input("Első játékos, add meg, hogy mit lépsz: ")
-        if first_player_input in ("rock","paper","scissors"):
+
+        while True:
+            first_player_input=input("Első játékos, add meg, hogy mit lépsz: ")
+            if first_player_input in ("rock","paper","scissors"):
                 break
     
-    while True:
-        second_player_input=input("Második játékos, add meg, hogy mit lépsz: ")
-        if second_player_input in ("rock","paper","scissors"):
-            break
+        while True:
+            second_player_input=input("Második játékos, add meg, hogy mit lépsz: ")
+            if second_player_input in ("rock","paper","scissors"):
+                break
 
-    if first_player_input == second_player_input:
-        print(f"\n Ez a kör döntetlen volt!\n")
-    elif (first_player_input == "rock" and second_player_input == "scissors") or (first_player_input == "paper" and second_player_input == "rock") or (first_player_input == "scissors" and second_player_input == "paper"):
-        first_player_score += 1
-        print(f"\n Ezt a kört az Első Játékos nyerte! Most {first_player_score} pontja van!\n")
-    else:
-        second_player_score += 1
-        print(f"\n Ezt a kört a Második Játékos nyerte! Most {second_player_score} pontja van!\n")
+        if first_player_input == second_player_input:
+            print(f"\n Ez a kör döntetlen volt! Próbáljuk meg újra! \n")
+        elif (first_player_input == "rock" and second_player_input == "scissors") or (first_player_input == "paper" and second_player_input == "rock") or (first_player_input == "scissors" and second_player_input == "paper"):
+            first_player_score += 1
+            print(f"\n Ezt a kört az Első Játékos nyerte! Most {first_player_score} pontja van!\n")
+            break
+        else:
+            second_player_score += 1
+            print(f"\n Ezt a kört a Második Játékos nyerte! Most {second_player_score} pontja van!\n")
+            break
     
 
 if first_player_score > second_player_score:
     print(f"\n Az Első Játékos nyerte ezt a meccset! \n Az Első Játékosnak {first_player_score} pontja volt, míg a Második Játékosnak {second_player_score} pontja volt. \n")
-elif first_player_score < second_player_score:
-    print(f"\n A Második Játékost nyerte ezt a meccset! \n Az Első Játékosnak {first_player_score} pontja volt, míg a Második Játékosnak {second_player_score} pontja volt. \n")
 else:
-    print(f"\n A meccs döntetlen volt! \n Az Első Játékosnak {first_player_score} pontja volt, míg a Második Játékosnak is {second_player_score} pontja volt. \n")
+    print(f"\n A Második Játékost nyerte ezt a meccset! \n Az Első Játékosnak {first_player_score} pontja volt, míg a Második Játékosnak {second_player_score} pontja volt. \n")
