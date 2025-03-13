75014c43defdf233e6531a0936ef5ca529f9137d
tiborkovics <tibor.kovics@gmail.com>
2025-03-13 10:01:44 +0100

    hianyzo hazi
    Hello itt egy kicsit elvesztek az adatok, ez a hazi mar regen kesz van. Most megprobalom pusholni vagy mi

--- homeworks/tiborkovics/hw_02_vars_datatypes/user_info.py
+++ homeworks/tiborkovics/hw_02_vars_datatypes/user_info.py
@@ -20,6 +20,7 @@ user_info = {
 
 languages = input("Kerlek adj meg 4 prog nyelvet").split(",")   # 1/1
 
+<<<<<<< Updated upstream
 user_info["skills"] = languages # 1/2
 print(user_info)
 
@@ -52,6 +53,29 @@ print(user_info)
 
 user_info["phone_contacts"]["Anyu"] = ["+36701455098" , "+36707884956"] # 10
 print(user_info)
+=======
+skills = list[languages] # 1/2
+
+languages = user_info["skills"] # 1/3
+
+user_info["favourite_meals"].sort()  # 2
+
+print(user_info["favourite_meals"][-1])  # 3
+
+
+user_info["favourite_meals"].append("spaghetti") # 4
+
+user_info["favourite_meals"].append("favourite_meals"[2:4])  # 5
+
+
+user_info["favourite_meals"] = set(user_info["favourite_meals"])
+
+user_info = list(user_info) # 6
+
+user_info["favourite_meals"][0]. # 7
+
+
+>>>>>>> Stashed changes
 
 
 
