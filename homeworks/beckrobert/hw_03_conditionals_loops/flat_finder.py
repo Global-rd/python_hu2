a815fe5fdaee66c43211419052d73af134232df4
robithesailor81 <beck4robert@gmail.com>
2025-03-13 12:05:00 +0100

    hw03_beckrobert_corrected

--- homeworks/beckrobert/hw_03_conditionals_loops/flat_finder.py
+++ homeworks/beckrobert/hw_03_conditionals_loops/flat_finder.py
@@ -7,7 +7,7 @@ if us_flat_city == "chicago":
 elif us_flat_city == "washington":
     print(f"you cannot rent the flat in {us_flat_city} for a price {us_flat_price}USD, because you hate the city")
 
-elif us_flat_city == ("new york" or "san francisco") and us_flat_price <=4000:
+elif us_flat_city in ("new york","san francisco") and us_flat_price <=4000:
     print(f"you can rent the flat in {us_flat_city} for a price {us_flat_price}USD")
 
 elif us_flat_price <=3000:
--- homeworks/beckrobert/hw_03_conditionals_loops/rock_paper_scissors.py
+++ homeworks/beckrobert/hw_03_conditionals_loops/rock_paper_scissors.py
@@ -9,45 +9,32 @@ player_1_score = 0
 player_2_score = 0
 
 while round < target_round:
-    player1_choice = input("player 1's choice: ")
-    player2_choice = input("player 2's choice: ")
 
-    if player1_choice == "paper" and player2_choice == "paper":
+    while True:
+        player1_choice = input("player 1's choice: ")
+        if player1_choice in ["rock", "paper", "scissors"]:
+            break
+        print("Invalid choice, please try again")
+
+    while True:
+        player2_choice = input("player 2's choice: ")
+        if player2_choice in ["rock", "paper", "scissors"]:
+            break
+        print("Invalid choice, please try again")
+
+    if player1_choice ==  player2_choice:
         print("it's a draw, choose again!")
-    elif player1_choice == "rock" and player2_choice == "rock":
-        print("it's a draw, choose again!")
-    elif player1_choice == "paper" and player2_choice == "paper":
-        print("it's a draw, choose again!")
-
-    elif player1_choice == "paper" and player2_choice == "rock":
-        player_1_score +=1
-        round+=1
-        print("it's one point to, player1")
-    elif player1_choice == "paper" and player2_choice == "scissors":
-        player_2_score +=1
-        round+=1
-        print("it's one point to, player2")
-    elif player1_choice == "rock" and player2_choice == "scissors":
-        player_1_score +=1
-        round+=1
-        print("it's one point to, player1")
-    elif player1_choice == "rock" and player2_choice == "paper":
-        player_2_score +=1
-        round+=1
-        print("it's one point to, player2")
-    elif player1_choice == "scissors" and player2_choice == "rock":
-        player_2_score +=1
-        round+=1
-        print("it's one point to, player2")
-    elif player1_choice == "scissors" and player2_choice == "paper":
-        player_1_score +=1
-        round+=1
-        print("it's one point to, player1")
-
-    else:
-        print("you have inserted an invalid choice")
-
     
+    elif ((player1_choice == "paper" and player2_choice == "rock") or
+          (player1_choice == "rock" and player2_choice == "scissors") or
+          (player1_choice == "scissors" and player2_choice == "paper")):
+        player_1_score +=1
+        round+=1
+        print("it's one point to, player1")
+    else:
+        player_2_score +=1
+        round+=1
+        print("it's one point to, player2") 
 
 print(f"player 1 has {player_1_score} point(s), player 2 has {player_2_score} point(s)")
 if player_1_score > player_2_score:
