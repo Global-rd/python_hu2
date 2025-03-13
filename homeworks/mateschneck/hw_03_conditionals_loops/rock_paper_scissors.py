a5fb639a4538541ce262012b54f11c5da2b11927
manzyka <mate.schneck@powerinnovate.hu>
2025-03-13 14:38:24 +0100

    hw3 jav2

--- homeworks/mateschneck/hw_03_conditionals_loops/rock_paper_scissors.py
+++ homeworks/mateschneck/hw_03_conditionals_loops/rock_paper_scissors.py
@@ -30,13 +30,12 @@ def determine_winner(choice1, choice2):
 
 def play_game():
     rounds = get_valid_rounds()
-    
-    while True:
-        score1 = 0
-        score2 = 0
-        
-        for _ in range(rounds):
-            print("\nÚj kör!")
+    score1 = 0
+    score2 = 0
+
+    for round_number in range(1, rounds + 1):  
+        while True:
+            print(f"\n{round_number}. kör!")  
             choice1 = get_valid_choice("1. játékos")
             choice2 = get_valid_choice("2. játékos")
             winner = determine_winner(choice1, choice2)
@@ -44,24 +43,19 @@ def play_game():
             if winner == 1:
                 print("1. játékos nyerte ezt a kört!")
                 score1 += 1
+                break  
             elif winner == 2:
                 print("2. játékos nyerte ezt a kört!")
                 score2 += 1
+                break  
             else:
                 print("Döntetlen, senki nem kap pontot!")
 
-        print("\nJáték vége!")
-        if score1 > score2:
-            print(f"Az 1. játékos nyert {score1} ponttal!")
-            break
-        elif score2 > score1:
-            print(f"A 2. játékos nyert {score2} ponttal!")
-            break
-        else:
-            print("A játék döntetlennel zárult! Döntő kör következik!")
-            # reset a döntő körhöz
-            score1 = 0
-            score2 = 0
+    print("\nJáték vége!")
+    if score1 > score2:
+        print(f"Az 1. játékos nyert {score1} ponttal!")
+    else:
+        print(f"A 2. játékos nyert {score2} ponttal!")
 
 if __name__ == "__main__":
     play_game()
