ab1b17f0ef0aced76919e51e9f6e37056dabbe15
RiksatBacsi <dani.varsanyi@gmail.com>
2025-03-24 13:50:52 +0100

    1
    1

--- homeworks/varsanyidaniel/hw_04_exceptions_logging/to_do.py
+++ homeworks/varsanyidaniel/hw_04_exceptions_logging/to_do.py
@@ -1,33 +1,36 @@
 def read():
     with open("homeworks/varsanyidaniel/hw_04_exceptions_logging/.txt" , "r") as file:
-        print(file)
-        """lines = file.readlines()
-        print(lines)
-        for line in lines:
-            print(line.strip())"""
+        for line in file.readlines():
+            print(line.strip())
 
 
+def add(addition):
+    with open("homeworks/varsanyidaniel/hw_04_exceptions_logging/.txt" , "a") as file:
+        file.write(addition , "\n")
 
 
-def display_menu ():
+def remove():
+
+
+def display_menu():
     print("\n 1. Add Task \n 2. Read Tasks \n 3 Remove Tasks \n 4. Exit \n")
 
-
+display_menu()
 while True:
-    display_menu()
     try:
         option=int(input("Mondd meg, mit választasz: "))
     except ValueError:
         print("\n Csakis 1, 2, 3 vagy 4 -est választhatsz! \n")
     else:
         if option == 1:
-            print("2")
+            addition=input("Írd le, hogy milyen teendőt szeretnél hozzáadni a programhoz:")
+            add(addition)
         elif option == 2:
             read()
         elif option == 3:
             print("2")
         elif option == 4:
-            print("2")
             break
         else:
             print("\n Csakis 1, 2, 3 vagy 4 -est választhatsz! \n")
+            display_menu()
