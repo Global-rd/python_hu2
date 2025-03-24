449702db6626eafc204b5b018f35ba8fe5bad0c1
aliptaka <aliptaka@gmail.com>
2025-03-24 20:15:27 +0100

    flat_finder_fine_tune

--- homeworks/liptakandras/hw_03_conditionals_loops/flat_finder.py
+++ homeworks/liptakandras/hw_03_conditionals_loops/flat_finder.py
@@ -2,23 +2,26 @@
 # INPUTOK
 
 city = str(input("Which city would you like to live in?"))  # str, hogy biztosan string legyen, de ebben nem vagyok biztos
-rent = int(input("What is the rental fee you are wondering to pay in USD?"))
+
+# A rental fee-t csak lentebb, a logika alapján bizonyos esetekben kérem be.
 
 city = city.lower()  # az értékek miatt csupa kisbetű, mert a bekért adatban lehet nagybetű is
 
 # FELTÉTELRENDSZER
 
-if city == "washington":  # Washington kizárása
-    print(f"Sarah wouldn't move to the city.")
-elif city == "chicago":  # Chicago-ba bármi áron beköltözne
-    print(f"Sarah would like to rent the apartment.")
+if city == "washington":  # Washington kizárása -> így nem kérek be rental fee-t
+    print(f"Sarah, you wouldn't move to any apartment in this city.")
+elif city == "chicago":  # Chicago-ba bármi áron beköltözne -> így nem kérek be rental fee-t
+    print(f"Sarah, you would love to rent the apartment in Chicago.")
 elif city == "new york" or city == "san francisco":
+    rent = int(input("What is the rental fee of the apartment in USD?"))
     if rent < 4000:
-        print(f"Sarah would like to rent the apartment.")
+        print(f"Sarah, this apartment matches with your requiremnets.")
     else:
-        print(f"Sarah finds the rent too high.")
+        print(f"Sarah the rental fee is higher than your ideal price.")
 else:
+    rent = int(input("What is the rental fee of the apartment in USD?"))
     if rent < 3000:
-        print(f"Sarah would like to rent the apartment")
+        print(f"Sarah, this apartment matches with your requiremnets.")
     else:
-        print(f"Sarah finds the rent too high")
+        print(f"Sarah the rental fee is higher than your ideal price.")
