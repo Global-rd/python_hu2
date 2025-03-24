64d9bddf33fbd4959eb5efa5f750636077b54a08
aliptaka <aliptaka@gmail.com>
2025-03-24 20:50:23 +0100

    rock-paper-scissors fine tune

--- homeworks/liptakandras/hw_03_conditionals_loops/rock_paper_scissors.py
+++ homeworks/liptakandras/hw_03_conditionals_loops/rock_paper_scissors.py
@@ -29,17 +29,23 @@ while rounds < rounds_selected_by_players:  # amint egyenlők, már nincs több 
     
     # JÁTÉKOSOKNAK VÁLASZTANIUK KELL
 
-    print(f"Select your weapon!")
+    print(f"Player 1: {player_1_points} points - Player 2: {player_2_points} points")
+    print(f"ROUND {rounds + 1}! Select your weapon!")
 
-    player_1_weapon = input("Rock, Paper or scissors?").lower()  # lower az összehasonlíthatóság miatt
-    player_2_weapon = input("Rock, Paper or scissors?").lower()
+    player_1_weapon = input("Player 1, rock, Paper or scissors?").lower()  # lower az összehasonlíthatóság miatt
+    while player_1_weapon not in ["rock", "paper", "scissors"]:
+        player_1_weapon = input("Player 1, you must choose a valid weapon. Rock, Paper or scissors?").lower()
+
+    player_2_weapon = input("Player 2, Rock, Paper or scissors?").lower()
+    while player_2_weapon not in ["rock", "paper", "scissors"]:
+        player_2_weapon = input("Player 2, you must choose a valid weapon. Rock, Paper or scissors?").lower()
 
     # HA NEM VALID FEGYVERT VÁLASZTOTT VALAMELYIK JÁTÉKOS
 
-    if player_1_weapon not in ["rock", "paper", "scissors"] or player_2_weapon not in ["rock", "paper", "scissors"]:
-        print(f"Invalid weapon! You both have to choose rock, paper or scissors!")
+   # if player_1_weapon not in ["rock", "paper", "scissors"] or player_2_weapon not in ["rock", "paper", "scissors"]:
+    #    print(f"Invalid weapon! You both have to choose rock, paper or scissors!")
 
-        continue  # az invalid fegyverválasztás esetén újra kell kezdeni a loopot
+      #  continue  # az invalid fegyverválasztás esetén újra kell kezdeni a loopot
     
     # HA MINDKÉT JÁTÉKOS VALID FEGYVERT VÁLASZTOTT
 
@@ -70,6 +76,6 @@ while rounds < rounds_selected_by_players:  # amint egyenlők, már nincs több 
 #  HA A JÁTÉK VÉGET ÉRT:
     
 if player_1_points > player_2_points:
-    print(f"Player 1 won by {player_1_points - player_2_points} points.")
+    print(f"GAME OVER! Player 1 won by {player_1_points - player_2_points} points.")
 else:
-    print(f"Player 2 won by {player_2_points - player_1_points} points.")
+    print(f"GAME OVER! Player 2 won by {player_2_points - player_1_points} points.")
