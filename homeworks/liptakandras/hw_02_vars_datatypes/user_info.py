eccc1dbc54632948475b4100ef82e1064f265562
aliptaka <aliptaka@gmail.com>
2025-03-24 17:52:23 +0100

    small changes in code

--- homeworks/liptakandras/hw_02_vars_datatypes/user_info.py
+++ homeworks/liptakandras/hw_02_vars_datatypes/user_info.py
@@ -21,15 +21,17 @@ user_info = {
 
 # 1. 4 PROGRAMOZÁSI NYELV BEKÉRÉSE
 
-description = "Please add 4 programming languages you have experience with!"  # instrukció és beviteli mezők a felhasználónak
+print("Please add 4 programming languages you have experience with!")  # instrukció és beviteli mezők a felhasználónak
 programming_language_1 = input("1st programming language: ")
 programming_language_2 = input("2nd programming language: ")
 programming_language_3 = input("3rd programming language: ")
 programming_language_4 = input("4th programming language: ")
 
 string_of_programming_languages = f"{programming_language_1},{programming_language_2},{programming_language_3},{programming_language_4}"  # egyesítés egy stringbe, vesszővel elválaszta, szóközök nélkül
+print(string_of_programming_languages)
 
 list_of_programming_languages = string_of_programming_languages.split(",")  # string konvertálása listává a vesszők mentén
+print(list_of_programming_languages)
 
 user_info["skills"] = list_of_programming_languages
 
@@ -37,16 +39,17 @@ user_info["skills"] = list_of_programming_languages
 # 2. FAVORITE MEALS ELEMEK RENDEZÉSE
 
 user_info["favourite_meals"].sort()
+print(user_info["favourite_meals"])
 
 
 # 3. FAVOURITE MEALS LISTA UTOLSÓ ELŐTTI ELEMÉNEK KIPRINTELÉSE
 
-print(user_info["favourite_meals"][-2])
+print(user_info["favourite_meals"][-1])
 
 
 # 4. HOZZÁADÁS A LISTÁHOZ
 
-user_info["favourite_meals"].append["spaghetti"]
+user_info["favourite_meals"].append("spaghetti")
 
 
 # 5. 3-4 ELEMEK HOZZÁADÁSA ÚJRA A LISTÁHOZ
@@ -56,6 +59,7 @@ user_info["favourite_meals"].append
 list_elements = user_info["favourite_meals"][2:4]
 
 user_info["favourite_meals"].extend(list_elements)
+print(user_info["favourite_meals"])
 
 
 # 6. DUPLIKÁTUMOK TÖRLÉSE
