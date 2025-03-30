ee8fa0bae78df89c268f5f02da97ba8f85bedf9d
Ladislav77 <gonczol.laci@gmail.com>
2025-03-30 22:33:02 +0200

    Update to_do.py

--- homeworks/gonczolladislav/hw_04_exceptions_logging/to_do.py
+++ homeworks/gonczolladislav/hw_04_exceptions_logging/to_do.py
@@ -20,45 +20,6 @@ logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 logger.setLevel(logging.DEBUG)
 
-#logger.debug("Debug message")
-#logger.info("Info message")
-#logger.warning("Warning message")
-#logger.error("Error message")
-#logger.critical("Critical message")
-
-def create_file_handler(log_file, level):
-    file_handler = logging.FileHandler(log_file)
-    file_handler.setLevel(level)
-    return file_handler
-
-def create_stream_handler(level):
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(level)
-    return stream_handler
-
-def set_formatter(handler):
-    log_format = logging.Formatter('%(asctime)s - %(name)s - %(module)s - %(levelname)s - %(message)s')
-    handler.setFormatter(log_format)
-
-def setup_logger(name, log_file='logfile.log', level=logging.DEBUG, handlers=None):
-
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-
-    if handlers is None:
-        handlers = ['file', 'stream']
-
-    for handler in handlers:
-        if handler == 'file':
-            file_handler = create_file_handler(log_file=log_file, level=level)
-            set_formatter(file_handler)
-            logger.addHandler(file_handler)
-        elif handler == 'stream':
-            stream_handler = create_stream_handler(level)
-            set_formatter(stream_handler)
-            logger.addHandler(stream_handler)
-    return logger
-
 # setting a file name as constant
 FILE_NAME = "to_do_list.txt" 
 
