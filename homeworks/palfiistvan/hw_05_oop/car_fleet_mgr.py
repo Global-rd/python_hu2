ff12550ff18492b953feb779aa19b193e5098589
palfiistvan68 <palfiistvan68@gmail.com>
2025-03-30 16:43:17 +0200

    palfiistvan-hw_05_oop_phase-01

---
+++ homeworks/palfiistvan/hw_05_oop/car_fleet_mgr.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, brand:str, model:str, year:int)
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.mileage = 0
+        self.fuel_level = 100
+    
+    def drive(self, km):
+        fuel_usage
