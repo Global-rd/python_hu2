61b7161048692e7f4b6d55805bb19190b0f0709d
Reechardo <ritchoo96@gmail.com>
2025-03-31 12:30:50 +0200

    class Car done
    A Car class tesztelve, készen

--- homeworks/horvathrichard/hw_05_oop/car_fleet_mgr.py
+++ homeworks/horvathrichard/hw_05_oop/car_fleet_mgr.py
@@ -3,37 +3,51 @@
 class Car:
 
     #a drive metódus által levezetett kilométerek hozzáadása minden kocsi esetében(!)
-    sum_mileage = []
+    sum_mileage = 0
     
     #konstruktor az attribútumok beállításaira
-    def __init__(self, brand:str, model:str, year:int, mileage=float(0), fuel_level=float(100)):
+    def __init__(self, brand:str, model:str, year:int, mileage=float(0.00), fuel_level=float(100.00)):
         self.brand = brand
         self.model = model
         self.year = year
         self.mileage = mileage
         self.fuel_level = fuel_level
 
+    #a létrehozott autó felhasználóbarát nevesítése
+    def __str__(self):
+        return(f"{self.brand} {self.model}, {self.year}")
+
     #adott számú kilométerrel növeli a kilométeróra állását, és csökkenti az üzemanyag-szintet(százalékban) -> 0.1/km
-    def drive (self, mileage):
-        self.mileage += mileage
+    def drive (self, mileage=float):
         consume = float(mileage * 0.1)
-        if self.fuel_level > 0:
+        if self.fuel_level - consume > 0:
+            Car.sum_mileage += mileage
+            self.mileage += mileage
             self.fuel_level -= consume
-            return self.fuel_level
-        print(f"{mileage} km was driven by {self.model}.\n---Odometer reading: {self.mileage}---\nThe car consumed {consume}l fuel.\n---Fuel level: {self.fuel_level}%---")
-        if self.fuel_level < 50:
-            print(f"Your fuel level is under it's half!")
-        elif self.fuel_level <25:
-            print(f"--------Warning!--------\nYour fuel level is on low: {self.fuel_level}!")
+            print(f"{mileage} km was driven by the {self.model}.\n---Odometer reading: {self.mileage}---\nThe car consumed {consume:.2f}l fuel.\n---Fuel level: {self.fuel_level:.2f}%---")
+            if self.fuel_level <25:
+                print(f"--------Warning!--------\nYour fuel level is on low: {self.fuel_level:.2f}!")
+            elif self.fuel_level < 50:
+                print(f"Your fuel level is under it's half!")
+        else:
+            print(f"You can not travel {mileage}km with {self.fuel_level:.2f}L fuel!\nTime to refuel!")
+        
     
 
     #feltölti az üzemanyag-szintet egy adott mennyiséggel
     def refuel (self, fuel_load):
-        if self.fuel_level < 100 and self.fuel_level + fuel_load < 100:
+        if self.fuel_level + fuel_load < 100:
             self.fuel_level += fuel_load
             print(f"{self.model} was loaded with {fuel_load}L of fuel successfully!\n---It's fuel level is on {self.fuel_level}%---")
+        else:
+            print(f"You can not overload your fuel tank!")
 
+    #az egész flotta által megtett kilométerek kiíratása
+    @classmethod
+    def get_sum_mileage(cls):
+        return cls.sum_mileage
 
+"""
 #a flotta
 class Fleet:
 
@@ -57,5 +71,17 @@ class Fleet:
     #az egész flotta megtett kilométere összesítve
     def fleet_mileage (self,):
         pass
-
-
+"""
+car1 = Car("Porsche","GT",1996,0)
+print(car1)
+print(car1.mileage)
+print(car1.fuel_level)
+print("-----")
+car1.drive(33.8)
+print("-----")
+print(car1.fuel_level)
+car1.drive(500)
+car1.drive(400)
+car1.drive(400)
+car1.refuel(99)
+print(Car.get_sum_mileage())
