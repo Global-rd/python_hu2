36ff1c88371f8fad9adbc613c8f9956d18f44d38
NagyIstvanGabor <nagy.istvan.gabor@protonmail.com>
2025-04-01 19:37:43 +0200

    lesson 11 done

---
+++ lessons/lesson_11/basics.py
@@ -0,0 +1,5 @@
+#real life example: építünk egy ikerházat egy adott telken
+
+#program: a ház építési terve, nem használ semmilyen erőforrást amíg el nem kezdődik az építkezés
+#process: a ház tényleges építési folyamata, már erőforrásokat használva. Ikerház miatt lehet a két oldal 2 különböző process
+#thread: falazás, festés, elektronika, víz/gáz szerelés stb. egy process-en belül, vagyis egy ház-oldalon belül.
