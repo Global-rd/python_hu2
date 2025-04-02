97bac1c3b50a7dcb772e7973241e231375892d56
Hajni123 <hajni.kiraly90@gmail.com>
2025-04-02 19:49:52 +0200

    2. módosítás

--- homeworks/kiralyhajnalka/hw_03_conditionals_loops/rock_paper_scissors.py
+++ homeworks/kiralyhajnalka/hw_03_conditionals_loops/rock_paper_scissors.py
@@ -1,4 +1,4 @@
-# 2. feladat: while és for loop használata
+# 2. feladat javított verzió
 
 while True:
     try:
@@ -15,14 +15,11 @@ while True:
         print("Hiba: Kérlek, adj meg egy érvényes számot!")
 
 choices = ["rock", "paper", "scissors"]
-
 player1_score = 0
 player2_score = 0
 
 for round_num in range(1, rounds + 1):
     print(f"\n{round_num}. kör:")
-
-while True:  
 
     while True:
         choice1 = input("Első játékos választ (rock, paper, scissors): ").lower()
@@ -30,29 +27,47 @@ while True:
             break
         else:
             print("Hiba: Csak 'rock', 'paper' vagy 'scissors' választható!")
-    
+
     while True:
         choice2 = input("Második játékos választ (rock, paper, scissors): ").lower()
         if choice2 in choices:
             break
         else:
             print("Hiba: Csak 'rock', 'paper' vagy 'scissors' választható!")
-    
+
     if choice1 == choice2:
-        print("Döntetlen!")
-        continue
-    elif (choice1 == "rock" and choice2 == "scissors") or \
-         (choice1 == "scissors" and choice2 == "paper") or \
-         (choice1 == "paper" and choice2 == "rock"):
+        print("Döntetlen! Újra kell játszani ezt a kört!")
+        while True:
+            choice1 = input("Első játékos választ (rock, paper, scissors): ").lower()
+            if choice1 in choices:
+                break
+            else:
+                print("Hiba: Csak 'rock', 'paper' vagy 'scissors' választható!")
+
+        while True:
+            choice2 = input("Második játékos választ (rock, paper, scissors): ").lower()
+            if choice2 in choices:
+                break
+            else:
+                print("Hiba: Csak 'rock', 'paper' vagy 'scissors' választható!")
+
+        if choice1 == choice2:
+            print("Ismét döntetlen! Újraindul a kör.")
+            continue  # Újraindítja az aktuális kört, ha ismét döntetlen
+
+    if (choice1 == "rock" and choice2 == "scissors") or \
+       (choice1 == "scissors" and choice2 == "paper") or \
+       (choice1 == "paper" and choice2 == "rock"):
         print("Első játékos nyerte a kört!")
         player1_score += 1
     else:
         print("Második játékos nyerte a kört!")
         player2_score += 1
-    break
 
 print("\nJáték vége!")
 if player1_score > player2_score:
     print(f"Az első játékos nyert {player1_score} ponttal!")
-else:
+elif player2_score > player1_score:
     print(f"A második játékos nyert {player2_score} ponttal!")
+else:
+    print("A játék döntetlennel zárult!")
