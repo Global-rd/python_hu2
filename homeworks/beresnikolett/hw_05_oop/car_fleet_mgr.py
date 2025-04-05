2c0c6deef3962e3e43a358603d39666e0ae5d54f
beresniki <beresniki1996@gmail.com>
2025-04-05 09:59:30 +0200

    beresnikolett/homework-05 changes

--- homeworks/beresnikolett/hw_05_oop/car_fleet_mgr.py
+++ homeworks/beresnikolett/hw_05_oop/car_fleet_mgr.py
@@ -8,7 +8,7 @@ class Car:
 
     def drive(self, distance: float):
         if distance <= 0:
-            print("Cannot drive a non-positive distance.")
+            raise ValueError("Cannot drive a non-positive distance.")
             return
         fuel_for_drive = distance * 0.1  # 0.1% üzemanyag
         max_distance = self.fuel_level / 0.1
@@ -19,22 +19,22 @@ class Car:
         else:    
             self.mileage += distance
             self.fuel_level -= fuel_for_drive
-            return print(f"The {self.brand} {self.model} has driven {distance} kilometers. Current fuel level: {self.fuel_level}%.")
+            print(f"The {self.brand} {self.model} has driven {distance} kilometers. Current fuel level: {self.fuel_level}%.")
     
     def refuel(self, amount: float):
         if amount <= 0:
-            return print("The amount of fuel to be added must be positive.")
+            print("The amount of fuel to be added must be positive.")
         elif self.fuel_level + amount > 100:
             self.fuel_level = 100
-            return print(f"The {self.brand} {self.model}'s fuel tank is full, no need for more fuel. Current fuel level: {self.fuel_level}%.")
+            print(f"The {self.brand} {self.model}'s fuel tank is full, no need for more fuel. Current fuel level: {self.fuel_level}%.")
         else:
             self.fuel_level += amount
-            return print(f"The {self.brand} {self.model} has been refueled with {amount}%. Current fuel level: {self.fuel_level}%.")
+            print(f"The {self.brand} {self.model} has been refueled with {amount}%. Current fuel level: {self.fuel_level}%.")
         
     def __str__(self):
-        return print(f"{self.year} {self.brand} {self.model} - {self.mileage} kilometers driven, {self.fuel_level}% of fuel left.")
+        return f"{self.year} {self.brand} {self.model} - {self.mileage} kilometers driven, {self.fuel_level}% of fuel left."
     
-    def __lens__(self):
+    def __len__(self):
         return len(self.brand) + len(self.model) + len(str(self.year)) + len(str(self.milage)) + len(str(self.fuel_level))
     
 class Fleet(Car):
@@ -42,27 +42,27 @@ class Fleet(Car):
         self.cars = []
       
     def __str__(self):
-        return print(f"Fleet contains {len(self.cars)} cars.")
+        print(f"Fleet contains {len(self.cars)} cars.")
     
     def list_of_cars(self):
         for cars in self.cars:
             print(f"{cars.brand} {cars.model}")
-        return print(f"Fleet contains {len(self.cars)} cars.")
+        print(f"Fleet contains {len(self.cars)} cars.")
         
     def add_car(self, car):
         self.cars.append(car)
-        return print(f"{car.brand} {car.model} has been added to the fleet.")
+        print(f"{car.brand} {car.model} has been added to the fleet.")
     
     def remove_car(self, car):
         if car in self.cars:
             self.cars.remove(car)
-            return print(f"{car.brand} {car.model} has been removed from the fleet.")
+            print(f"{car.brand} {car.model} has been removed from the fleet.")
         else:
-            return print(f"{car.brand} {car.model} is not in the fleet.")
+            print(f"{car.brand} {car.model} is not in the fleet.")
         
     def fleet_total_mileage(self):
         total_mileage = sum(car.mileage for car in self.cars)
-        return print(f"The total mileage of the fleet is {total_mileage} kilometers.")
+        print(f"The total mileage of the fleet is {total_mileage} kilometers.")
     
     def fleet_status(self):
         status = []
