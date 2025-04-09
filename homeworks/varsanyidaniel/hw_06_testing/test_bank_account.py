6f801495d91c98098cf0c1b4ae33c9e2e416bff0
RiksatBacsi <dani.varsanyi@gmail.com>
2025-04-09 18:31:04 +0200

    2
    2

--- homeworks/varsanyidaniel/hw_06_testing/test_bank_account.py
+++ homeworks/varsanyidaniel/hw_06_testing/test_bank_account.py
@@ -3,11 +3,15 @@ from bank_account import BankAccount
 
 @pytest.fixture
 def account_1():
-    account_1 = BankAccount("Thomas",100)
+    return BankAccount("Thomas",100)
 
 @pytest.fixture
 def account_2():
-    account_1 = BankAccount("Dexter")
+    return BankAccount("Dexter")
+
+def test_basic_inputs(account_1):
+    account_1.withdraw(50.0)
+    assert account_1.items["balance"] == 50
 
 
 @pytest.mark.parametrize("account_name, balance, expected_error", [
@@ -18,10 +22,6 @@ def test_invalid_account_creation(account_name, balance, expected_error):
     with pytest.raises(expected_error):
         BankAccount(account_name, balance)
 
-
-def test_basic_inputs(account_1):
-    account_1.withdraw(50)
-    assert account_1.items["balance"] == 50
 
 
 
