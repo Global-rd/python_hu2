c66c67d00c3f36ab96b6274b12874f9e9f097416
bergerandras <andras@berger.hu>
2025-04-10 10:16:57 +0200

    Update bank_account_test.py
    fixed!

--- homeworks/bergerandras/hw_06_testing/bank_account_test.py
+++ homeworks/bergerandras/hw_06_testing/bank_account_test.py
@@ -9,32 +9,32 @@ def account_01():
 def account_02():
     return BankAccount(owner="Dávid", balance=225)
 
-def invalid_account_creation_test():
+def test_invalid_account_creation():
     with pytest.raises(ValueError):
         BankAccount(owner="Sanyi", balance=-200)
 
 @pytest.mark.parametrize("amount", [-225, -75, 0])
-def deposit_invalid_amount_test(account_01, amount):
+def test_deposit_invalid_amount(account_01, amount):
     with pytest.raises(ValueError):
         account_01.deposit(amount)
 
-def account_01_deposit_test(account_01):
+def test_account_01_deposit(account_01):
     account_01.deposit(500)
     assert account_01.get_balance() == 2000
 
-def account_02_withdraw_test(account_02):
+def test_account_02_withdraw(account_02):
     account_02.withdraw(25)
     assert account_02.get_balance() == 200
 
-def account_02_invalid_withdraw_test(account_02):
+def test_account_02_invalid_withdraw(account_02):
     with pytest.raises(ValueError):
         account_02.withdraw(226)
 
-def transfer_test(account_01, account_02):
-    account_01.transfer(account_02, 500)
+def test_transfer(account_01, account_02):
+    account_01.transfer(500, account_02)
     assert account_01.get_balance() == 1000
     assert account_02.get_balance() == 725
 
-def invalid_account_transfer_test(valid_account):
+def test_invalid_account_transfer(account_01):
     with pytest.raises(TypeError):
-        valid_account.transfer("invalid name", 2000)
+        account_01.transfer("invalid name", 2000)
