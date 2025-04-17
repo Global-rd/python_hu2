2b8c0a63ffbd39d3a96d3e91e7b595a12c638879
Katieipacs <katieipacs@gmail.com>
2025-04-16 19:10:36 -0600

    Update Coin_gecko_df.py
    Javitott 7. hazi

--- homeworks/ipacskata/hw_07_extract_transform/Coin_gecko_df.py
+++ homeworks/ipacskata/hw_07_extract_transform/Coin_gecko_df.py
@@ -1,3 +1,7 @@
+import requests 
+import pandas as pd
+
+
 url = "https://api.coingecko.com/api/v3/coins/markets"
 
 
@@ -15,20 +19,24 @@ data = response.json()
 
 df=pd.DataFrame(data)
 print(df)
+
 print(df.info())
+print("Empty cells:")
+print(df.isnull().sum())
+
 sum_market_cap= df["market_cap"].sum()
 print(sum_market_cap)
 print(df.columns)
 top50_df = df.sort_values("current_price", ascending=False)
 print(top50_df.head(50)) 
-price_change_percentage_df = top50_df.sort_values("price_change_percentage_24h", ascending=True) 
+price_change_percentage_df = top50_df.sort_values("price_change_percentage_24h", ascending=True).head(50) 
 
 def change_direction(row):
     if row["price_change_percentage_24h"] > 0:
         return "+"
-    elif row["price_change_percentage_24h"]<0:
+    elif row["price_change_percentage_24h"] < 0:
         return "-"
     else:
-        return '0'
+        return "0"
 top50_df["change direction"] = top50_df.apply(change_direction, axis=1)
 print(top50_df)    
