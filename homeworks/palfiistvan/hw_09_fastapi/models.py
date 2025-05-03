392d13aa97c1574dd91629fa963b0abf847d30c7
palfiistvan68 <palfiistvan68@gmail.com>
2025-05-03 12:59:15 +0200

    palfiistvan_hw_09_fastapi_First-Try

---
+++ homeworks/palfiistvan/hw_09_fastapi/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import Mapped
+from uuid import uuid1, UUID
+from pydantic import BaseModel
+from typing import Optional
+from database import Base
+
+#database model
+class WsItem(Base):
+    __tablename__ = "Webshop"
+    id: Mapped[str] = Column(String, primary_key=True, default=lambda: str(uuid1()))
+    item_name: Mapped[str] = Column(String, nullable=False)
+    quantity: Mapped[int] = Column(Integer, nullable=False)
+    price: Mapped[int] = Column(Integer, nullable=False)
+    category: Mapped[str] = Column(String, nullable=False)
+
+#pydantic model
+class WsItemRequest(BaseModel):
+    item_name: str
+    quantity: int
+    price: int
+    category: Optional[str]
+
+class WsItemResponse(WsItemRequest):
+    id: UUID
+
