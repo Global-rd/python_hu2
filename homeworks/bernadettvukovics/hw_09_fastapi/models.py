d9cb96cc35cbe48dbd2b46d8b44faee2e6111768
bernadettvkvcs <bernadettvkvcs@gmail.com>
2025-05-04 10:32:00 +0200

    Update models.py

--- homeworks/bernadettvukovics/hw_09_fastapi/models.py
+++ homeworks/bernadettvukovics/hw_09_fastapi/models.py
@@ -7,7 +7,7 @@ from typing import Optional
 # SQLAlchemy model
 class Product(Base):
     __tablename__ = "products"
-    id = Column(String, primary_key=True, default=lambda: str(uuid.uuid1()))
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid1)
     item_name = Column(String, nullable=False)
     quantity = Column(Integer, nullable=False)
     price = Column(Integer, nullable=False)
