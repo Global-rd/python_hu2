c23e40642dd8e5495bb144eb1a253c39782c420d
NagyIstvanGabor <nagy.istvan.gabor@protonmail.com>
2025-05-06 19:46:05 +0200

    lesson 19 done

---
+++ lessons/lesson_19/auchan_selectors.py
@@ -0,0 +1,14 @@
+def get_delivery_popup_close_button_xpath() -> str:
+    return '//*[@id="pge_cUz9766seB"]/div[1]/span'
+
+def get_accept_cookies_button_xpath() ->str:
+    return '//*[@id="onetrust-accept-btn-handler"]'
+
+def get_category_hierarchy_xpath() -> str:
+    return '//*[@id="container"]/div/div/div/div/div/div/div/nav/span'
+
+def get_product_name_xpath() -> str:
+    return '//*[@id="productDetails"]/div[1]/div[1]/div[1]/div[1]/h1'
+
+def get_product_price_class_name() -> str:
+    return 'FBX6R-Sn'
