733cdd5940dd73034844d826a0c1b850f41b0967
RiksatBacsi <dani.varsanyi@gmail.com>
2025-05-09 15:48:55 +0200

    5
    5

--- homeworks/varsanyidaniel/hw_09_fastapi/fastapi_app.py
+++ homeworks/varsanyidaniel/hw_09_fastapi/fastapi_app.py
@@ -14,3 +14,52 @@ async def lifespan(app: FastAPI):
     yield 
 
 app = FastAPI(lifespan=lifespan)
+
+
+
+@app.get("/products/", response_model=List[ProductResponse])
+async def get_all_products(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Product))
+    products = result.scalars().all()
+    return products
+
+@app.get("/products/{product_id}", response_model=ProductResponse)
+async def get_product(product_id: UUID, db: AsyncSession = Depends(get_db)):
+    return await get_product_by_id(product_id, db)
+
+@app.post("/products/", response_model = ProductResponse)
+async def add_product(product:ProductRequest, db: AsyncSession = Depends(get_db)):
+    new_product=Product(**product.model_dump())
+    db.add(new_product)
+    await db.commit()
+    await db.refresh(new_product)
+    return new_product
+
+@app.put("/products/{product_id}", response_model=ProductResponse)
+async def update_product(product_id: UUID, product_update: ProductRequest, db: AsyncSession = Depends(get_db)):
+    product = await get_product_by_id(product_id, db)
+    for key, value in product_update.model_dump(exclude_unset=True).items():
+        setattr(product, key, value)
+
+    db.add(product)
+    await db.commit()
+    await db.refresh(product)
+    return product
+
+@app.delete("/products/{product_id}", response_model=ProductResponse)
+async def delete_movie(product_id: UUID, db: AsyncSession = Depends(get_db)):
+    product = await get_product_by_id(product_id, db)
+
+    await db.delete(product)
+    await db.commit()
+
+    return product
+
+
+
+async def get_product_by_id(product_id: UUID, db:AsyncSession):
+    result = await db.execute(select(Product).where(Product.id == str(product_id)))
+    product = result.scalar_one_or_none() 
+    if not product:
+        raise HTTPException(status_code=404, detail=f"Error 404, Product ID {product_id} not found.")
+    return product
