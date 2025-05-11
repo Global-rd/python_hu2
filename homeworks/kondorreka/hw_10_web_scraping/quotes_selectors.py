828c98ba424c7cf9e64e30368f36cd816633078a
ladytoxi <kondorreka@gmail.com>
2025-05-11 20:13:21 +0200

    kondorreka\hw_10_web_scraping
    Szia, tudom, hogy nem jó, de már csak rontani sikerült rajta. :-(

---
+++ homeworks/kondorreka/hw_10_web_scraping/quotes_selectors.py
@@ -0,0 +1,21 @@
+#Top tagek megkeresése
+def get_top_tags_xpath() -> str:
+    return '/html/body/div/div/div/span/a'
+
+#összes blokk megkereséde
+def get_quotes_block_xpath() -> str:
+    #return '//div[@class="quote"]'
+    return '/html/body/div/div[2]/div[1]/div[2]'
+
+
+# Blokból idézet szövege
+def get_quote_text_xpath() -> str:
+    return '/html/body/div/div[2]/div[1]/div/span[1]'
+
+#blokkból a szerző neve
+def get_quote_author_xpath() -> str:
+    return '/html/body/div/div[2]/div[1]/div[2]/span[2]/small'
+
+#blokkból a tag
+def get_quote_tag_xpath() -> str:
+    return '/html/body/div/div[2]/div[1]/div[2]/div/a'
