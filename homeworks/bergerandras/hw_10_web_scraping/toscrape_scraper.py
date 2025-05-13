585178ce262b6bb95d92f01624cac40cce7c456e
bergerandras <andras@berger.hu>
2025-05-13 18:31:32 +0200

    Update toscrape_scraper.py

--- homeworks/bergerandras/hw_10_web_scraping/toscrape_scraper.py
+++ homeworks/bergerandras/hw_10_web_scraping/toscrape_scraper.py
@@ -5,6 +5,33 @@ from selenium.webdriver.common.by import By
 
 import toscrape_selectors as TSS
 
+def scrape_next_page(driver, quotes_list: list, authors_list: list, tags_list: list, tag, timeout=10):
+
+    next_button = driver.find_element(by=By.XPATH, value=TSS.get_next_button_xpath())
+
+    next_button.click()
+
+    WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.XPATH, TSS.get_quotes_div_xpath())))
+
+    quotes_div = driver.find_elements(by=By.XPATH, value=TSS.get_quotes_div_xpath())
+
+    for quote_div in quotes_div:
+                    
+        quote = quote_div.find_element(by=By.XPATH, value='./span[1]')
+        author = quote_div.find_element(by=By.XPATH, value='./span[2]/small')
+
+        quotes_list.append(quote.text.strip())
+        authors_list.append(author.text.strip())
+        tags_list.append(tag)
+
+    try:
+
+        scrape_next_page(driver, quotes_list, authors_list, tags_list, tag)
+
+    except:
+
+        return [quotes_list, authors_list, tags_list]
+
 def scrape_top10_tags(driver, timeout=10):
 
     tags = []
@@ -55,23 +82,8 @@ def scrape_authors_quotes_tags(driver, tags: list, timeout=10):
                 tags_list.append(tag)
 
             try:
-
-                next_button = driver.find_element(by=By.XPATH, value=TSS.get_next_button_xpath())
-
-                next_button.click()
-
-                WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.XPATH, TSS.get_quotes_div_xpath())))
-
-                quotes_div = driver.find_elements(by=By.XPATH, value=TSS.get_quotes_div_xpath())
-
-                for quote_div in quotes_div:
-
-                    quote = quote_div.find_element(by=By.XPATH, value='./span[1]').text.strip()
-                    author = quote_div.find_element(by=By.XPATH, value='./span[2]/small').text.strip()
-
-                    quotes_list.append(quote)
-                    authors_list.append(author)
-                    tags_list.append(tag)
+                
+                quotes_list, authors_list, tags_list = scrape_next_page(driver, quotes_list, authors_list, tags_list, tag, timeout=10)
 
             except:
 
